a1d7a85c04fc59c03dbba062526159448aec006a
Yuxi Dai <yuxid5@uci.edu>
2024-04-19 19:30:28 -0700

    try to append url to list

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup #this import is use to parsing html
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,12 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    links_collection = []
+    for n_url in resp.raw_response.url:
+        if is_valid(url):
+            links_collection.append(url)
+
+    return links_collection
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
